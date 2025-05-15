a9e5a6f1a730dd5d05fc89456e2fdfff6bd9a369
Luxibre <leverriernicolas@hotmail.fr>
2025-05-15 20:12:56 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('Luxibre Alpha script loaded');
+
+document.addEventListener("DOMContentLoaded", () => {
+  const canvas = document.getElementById("chart");
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "#00ff88";
+  ctx.fillRect(0, 150, 400, 50);
+});
