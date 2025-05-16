0aef5ee32fc0cd0db952797079605415370ae7b7
Luxibre <leverriernicolas@hotmail.fr>
2025-05-16 07:10:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById("chart");
   const ctx = canvas.getContext("2d");
   ctx.fillStyle = "#00ff88";
-  ctx.fillRect(0, 150, 400, 50);
+  ctx.fillRect(10, 100, 100, 30);
 });
