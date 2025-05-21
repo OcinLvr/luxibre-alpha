25d037c01ad52c98e987c390f69b32f665fea229
Luxibre <leverriernicolas@hotmail.fr>
2025-05-21 21:05:35 +0200

    Update auth-check.js

--- JS/auth-check.js
+++ JS/auth-check.js
@@ -1,9 +1,12 @@
 netlifyIdentity.on("init", (user) => {
   if (!user && window.location.pathname !== "/index.html") {
     alert("Veuillez vous connecter pour accéder à cette page.");
-    netlifyIdentity.open(); // Ouvre la fenêtre de login
+    setTimeout(() => {
+      netlifyIdentity.open(); // Affiche la fenêtre de connexion
+    }, 200); // Léger délai pour contourner le blocage navigateur
+
     netlifyIdentity.on("login", () => {
-      window.location.reload(); // Recharge après connexion
+      window.location.reload(); // Recharge la page après connexion
     });
   }
 });
