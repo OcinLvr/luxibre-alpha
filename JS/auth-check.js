a3e6d1de5ff54c3e6fd31bb9be1c8357f808579e
Luxibre <leverriernicolas@hotmail.fr>
2025-05-21 07:33:18 +0200

    Update auth-check.js

--- JS/auth-check.js
+++ JS/auth-check.js
@@ -1,16 +1,7 @@
-// js/auth-check.js
-
-document.addEventListener("DOMContentLoaded", () => {
-  const userData = JSON.parse(localStorage.getItem("luxibre_user"));
-
-  if (!userData || !userData.isAuthenticated) {
-    // Redirige les utilisateurs non connectés vers la page de connexion
-    window.location.href = "/login.html";
-  }
-
-  // Si l'utilisateur est connecté mais n'est pas abonné
-  if (!userData.isPremium) {
-    // Redirige les utilisateurs non premium vers la page d’abonnement
-    window.location.href = "/pricing.html";
-  }
-});
+if (window.netlifyIdentity) {
+  netlifyIdentity.on("init", (user) => {
+    if (!user) {
+      window.location.href = "/login.html";
+    }
+  });
+}
