9efcb1a7b18817ba81cee75d434ddb19481d380f
Luxibre <leverriernicolas@hotmail.fr>
2025-06-14 13:06:34 +0200

    Update supabase.js

--- JS/supabase.js
+++ JS/supabase.js
@@ -1,12 +1,13 @@
-// Initialiser Supabase
+// Initialiser Supabase (en global pour toute l'app)
 const supabaseUrl = 'https://jrgdwozxcilasllpvikh.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpyZ2R3b3p4Y2lsYXNsbHB2aWtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4MjQ0NTEsImV4cCI6MjA2MzQwMDQ1MX0.S2oGP2rdtq1IkW-oH5mC8omm698PdCgQJtGVLlIFj3w';
-const supabase = window.supabase = window.Supabase.createClient(supabaseUrl, supabaseKey);
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpyZ2R3b3p4Y2lsYXNsbHB2aWtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4MjQ0NTEsImV4cCI6MjA2MzQwMDQ1MX0.S2oGP2rdtq1I6lKdX3hRk8nL6o7r2TtK1J3r4cMWgk8I';
+
+window.supabase = window.Supabase.createClient(supabaseUrl, supabaseKey);
 
 async function getUserInfo() {
-  const { data: { user }, error: userError } = await supabase.auth.getUser();
+  const { data: { user }, error: userError } = await window.supabase.auth.getUser();
   if (!user) return null;
-  const { data, error } = await supabase
+  const { data, error } = await window.supabase
     .from('users')
     .select('ispremium, name')
     .eq('id', user.id)
